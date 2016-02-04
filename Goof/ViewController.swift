0ea4aea6fba2dbbea0fa5d461c66c36ff72b7dc1
Julian Lee <Julian@julians-macbook-pro.local>
2016-02-04 14:58:43 -0800

    Initial Commit

---
+++ Goof/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goof
+//
+//  Created by Julian Lee on 2/4/16.
+//  Copyright © 2016 mobilemakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
